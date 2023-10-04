0902c2b51537e7199be94f733e07c09edbf3ce83
hank <taiwanhank0908@gmail.com>
2023-10-04 11:56:49 +0800

    Initial Commit

---
+++ ebook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ebook
+//
+//  Created by User19 on 2023/10/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ebook/ebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ebookApp.swift
+//  ebook
+//
+//  Created by User19 on 2023/10/4.
+//
+
+import SwiftUI
+
+@main
+struct ebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
